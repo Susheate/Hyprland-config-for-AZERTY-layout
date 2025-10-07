da79948423f12217c8b6e00f35a66d25d6deba78
Susheat <88378854+Susheate@users.noreply.github.com>
2025-10-07 19:26:23 +0200

    Fix relative paths, dependencies versions and zshs

--- install.sh
+++ install.sh
@@ -2,23 +2,23 @@
 
 
 echo "Installing packages"
-sudo pacman -Sy --needed --noconfirm zsh git base-devel curl fastfetch starship hyprland waybar brightnessctl pavucontrol nwg-displays kitty ttf-fira-code otf-font-awesome dolphin uwsm
+sudo pacman -S --needed --noconfirm zsh git base-devel curl fastfetch starship hyprland waybar brightnessctl pavucontrol nwg-displays kitty ttf-fira-code otf-font-awesome dolphin uwsm xdg-desktop-portal-hyprland xdg-desktop-portal
 
 
 echo "Copying hyprland.conf"
-cp -rf /hypr/hyprland.conf ~/.config/hypr
+cp -rf hypr/hyprland.conf ~/.config/hypr
 
 
 echo "Copying waybar.jsonc"
-cp -rf /waybar/waybar.jsonc ~/.config/waybar
-
-
-echo "Copying .zshrc"
-cp -rf /zsh/.zshrc ~/.config
+cp -rf waybar/config.jsonc ~/.config/waybar
 
 
 echo "Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+echo "Copying .zshrc"
+cp -rf zsh/.zshrc ~/.config
 
 
 echo "Installing yay"
