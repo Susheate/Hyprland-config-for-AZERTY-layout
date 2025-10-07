5d8a6f0cac93f53a9e0284fd1e32f5a768dc9943
Susheat <88378854+Susheate@users.noreply.github.com>
2025-10-07 20:40:55 +0200

    Zsh change echo install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Installing packages"
 sudo pacman -S --needed --noconfirm zsh git base-devel curl fastfetch starship hyprland waybar brightnessctl pavucontrol nwg-displays kitty ttf-fira-code otf-font-awesome dolphin uwsm xdg-desktop-portal-hyprland xdg-desktop-portal
 
 
-echo "Changing shell to ZSH"
+echo "Changing shell to Zsh"
 sudo chsh -s /bin/zsh
 
 
