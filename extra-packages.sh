18009626b0cf45ff93d833f45a31c5aac96b67b8
Susheat <88378854+Susheate@users.noreply.github.com>
2025-10-09 09:59:08 +0200

    Add files via upload

--- extra-packages.sh
+++ extra-packages.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 
-echo "Installing packages"
+echo 'Installing packages'
 yay -S --needed --noconfirm steam wine lutris prismlauncher wine-mono flatpak kdeconnect plasma tlp ufw ly qalculate-qt code micro obsidian btop obs-studio filelight
 
 
-echo "Enabling services (systemd)"
+echo 'Enabling services (systemd)'
 sudo systemctl enable tlp && sudo systemctl enable ufw
 
 
-echo "Installing AUR packages, check PKGBUILD"
+echo 'Installing AUR packages, check PKGBUILD'
 yay -S --needed librewolf-bin
 
 
-echo "Installing Flatpaks"
+echo 'Installing Flatpaks'
 flatpak install org.vinegarhq.Sober org.vinegarhq.Vinegar com.heroicgameslauncher.hgl
 flatpak override --user --filesystem=xdg-run/app/com.discordapp.Discord:create --filesystem=xdg-run/discord-ipc-0 --device=input org.vinegarhq.Sober
 
 
-echo "Installation complete"
+echo 'Installation complete'
