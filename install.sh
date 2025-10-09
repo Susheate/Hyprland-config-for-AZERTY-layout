214ce7ca15ffe314940a783b39348a2f20ee376e
Susheat <88378854+Susheate@users.noreply.github.com>
2025-10-09 09:57:47 +0200

    Add fastfetch config copy in install

--- install.sh
+++ install.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
 
-echo "Installing packages"
+echo 'Installing packages'
 sudo pacman -S --needed --noconfirm zsh git base-devel curl fastfetch starship hyprland waybar brightnessctl pavucontrol nwg-displays kitty ttf-fira-code otf-font-awesome dolphin uwsm xdg-desktop-portal-hyprland xdg-desktop-portal
 
 
-echo "Changing shell to Zsh"
-chsh -s /bin/zsh
+echo 'Changing shell to Zsh'
+sudo chsh -s /bin/zsh
+
+echo 'Copying the fastfetch config'
+cp -rf fastfetch/config.jsonc ~/.config/fastfetch
+
+echo 'Copying hyprland.conf'
+cp -rf hypr/hyprland.conf ~/.config/hypr
 
 
-echo "Copying hyprland.conf"
-cp -rf hypr ~/.config
+echo 'Copying the waybar config'
+cp -rf waybar/config.jsonc ~/.config/waybar
 
 
-echo "Copying waybar.jsonc"
-cp -rf waybar ~/.config
+echo 'Installing Oh My Zsh'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
-echo "Installing Oh My Zsh"
-if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-fi
+echo 'Copying .zshrc'
+cp -rf zsh/.zshrc ~/.config
 
 
-echo "Copying .zshrc"
-cp -rf zsh/.zshrc ~/.config/.zshrc
-
-
-echo "Installing yay"
+echo 'Installing yay'
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 
 
-echo "Installing AUR packages, check PKGBUILD"
+echo 'Installing AUR packages, check PKGBUILD'
 yay -S --needed zen-browser-bin
 
 
-echo "Installation complete"
+echo 'Installation complete'
