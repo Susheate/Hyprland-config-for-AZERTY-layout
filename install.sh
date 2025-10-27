8f8a1d0a6794159538e3b10e6f73ada46f818e4d
Susheat <88378854+Susheate@users.noreply.github.com>
2025-10-27 15:13:00 +0100

    Added formatting to install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 
-echo 'Installing packages and updating system'
+clear="\e[0m"
+bold="\e[1m"
+green="\e[32m"
+cyan="\e[36m"
+magenta="\e[35m"
 
+
+echo -e "${bold}Installing packages and updating system${clear}"
 if [ -d /bedrock/strata/arch ] && [ -d /bedrock/strata/artix ]; then
-	echo 'Bedrock Linux detected, artix and arch strata found'
+	echo -e "${green}Bedrock Linux${clear} detected, ${cyan}artix${bold}${clear} and ${cyan}arch ${magenta}strata${clear} found"
 	sudo pacman -Syu --needed zsh git base-devel curl fastfetch starship hyprland waybar brightnessctl pavucontrol kitty ttf-firacode-nerd dolphin xdg-desktop-portal-hyprland xdg-desktop-portal playerctl wofi hyprpaper blueman
 	sudo strat -r arch pacman -Syu --needed nwg-displays nwg-look
 
@@ -14,49 +20,52 @@ else
 fi
 
 
-echo 'Changing shell to Zsh'
+echo -e "${bold}Changing shell to Zsh${clear}"
 
 chsh -s /bin/zsh
 
 
-echo 'Copying fastfetch config'
+echo -e "${bold}Copying fastfetch config${clear}"
 
 cp -rf fastfetch ~/.config
 
 
-echo 'Copying hyprland.conf'
+echo -e "${bold}Copying hyprland.conf${clear}"
 
 cp -rf hypr ~/.config
 
 
-echo 'Copying waybar config'
+echo -e "${bold}Copying waybar config${clear}"
 
 cp -rf waybar ~/.config
 
 
-echo 'Installing Oh My Zsh'
-
+echo -e "${bold}Checking if Oh My Zsh has already been installed${clear}"
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+	echo -e "${bold}Installing Oh My Zsh${clear}"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+else
+	echo -e "${bold}Oh My Zsh is already installed, skipping${clear}"
 
 fi
 
 
-echo 'Copying .zshrc'
+echo -e "${bold}Copying .zshrc${clear}"
 
 cp -rf zsh/.zshrc ~/.zshrc
 
 
-echo 'Installing yay'
+echo -e "${bold}Installing yay${clear}"
 
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
-makepkg -si
+makepkg -si --needed
 
 
-echo 'Installing AUR packages, check PKGBUILD'
+echo -e "${bold}Installing AUR packages, check PKGBUILDs${clear}"
 
 yay -S --needed zen-browser-bin hyprshot-git
 
 
-echo 'Installation complete'
+echo -e "${bold}Installation complete${clear}"
