3855cdd5e1c04ad571b289cff17b890d7ffeaa38
Susheat <88378854+Susheate@users.noreply.github.com>
2025-10-30 16:28:55 +0100

    Detect and apply display configuration
    Add display configuration detection in install script

--- install.sh
+++ install.sh
@@ -41,6 +41,14 @@ echo -e "${bold}Copying hyprland.conf${clear}"
 cp -rf hypr ~/.config
 
 
+echo -e "${bold}Detecting and applying displays configuration${clear}"
+
+resolution=xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/'
+
+
+echo -e "${bold}Copying hyprland.conf${clear}"
+
+
 echo -e "${bold}Copying waybar config${clear}"
 
 cp -rf waybar ~/.config
