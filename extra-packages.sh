73f08e4e86d000bd4da6b8b40ad2267335b3457a
Susheat <88378854+Susheate@users.noreply.github.com>
2025-11-16 19:13:46 +0100

    revert the plasma change

--- extra-packages.sh
+++ extra-packages.sh
@@ -12,11 +12,11 @@ echo -e "${bold}Installing packages and updating system${clear}"
 
 if [ -d /bedrock/strata/arch ] && [ -d /bedrock/strata/artix ]; then
 	echo -e "${green}Bedrock Linux${clear} detected, ${cyan}artix${clear} and ${cyan}arch ${magenta}strata${clear} found"
-	sudo pacman -Syu --needed wine lutris prismlauncher flatpak plasma-desktop ufw qalculate-qt micro btop obs-studio filelight steam ark
+	sudo pacman -Syu --needed wine lutris prismlauncher flatpak plasma ufw qalculate-qt micro btop obs-studio filelight steam ark
 	sudo strat -r arch pacman -Syu --needed obsidian code
 
 else
-	pacman -Syu --needed lutris prismlauncher wine-mono flatpak plasma-desktop ufw qalculate-qt code micro obsidian btop obs-studio filelight steam ark
+	pacman -Syu --needed lutris prismlauncher wine-mono flatpak plasma ufw qalculate-qt code micro obsidian btop obs-studio filelight steam ark
 
 fi
 
