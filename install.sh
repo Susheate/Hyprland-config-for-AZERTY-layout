70918a707de38ec4626327a8607b7887c1073a5e
Susheat <88378854+Susheate@users.noreply.github.com>
2025-11-20 17:25:44 +0100

    Added clipboard history

--- install.sh
+++ install.sh
@@ -12,10 +12,10 @@ echo -e "${bold}Installing packages and updating system${clear}"
 if [ -d /bedrock/strata/arch ] && [ -d /bedrock/strata/artix ]; then
 	echo -e "${green}Bedrock Linux${clear} detected, ${cyan}artix${clear} and ${cyan}arch ${magenta}strata${clear} found"
 	sudo pacman -Syu --needed zsh git base-devel curl fastfetch starship hyprland waybar brightnessctl pavucontrol kitty ttf-firacode-nerd dolphin xdg-desktop-portal-hyprland xdg-desktop-portal playerctl wofi hyprpaper blueman hyprpicker nm-connection-editor hyprshot network-manager-applet breeze mako
-	sudo strat -r arch pacman -Syu --needed nwg-displays nwg-look archlinux-xdg-menu
+	sudo strat -r arch pacman -Syu --needed nwg-displays nwg-look archlinux-xdg-menu cliphist
 
 else
-	sudo pacman -Syu --needed zsh git base-devel curl fastfetch starship hyprland waybar brightnessctl pavucontrol nwg-displays kitty ttf-firacode-nerd dolphin xdg-desktop-portal-hyprland xdg-desktop-portal playerctl wofi hyprpaper blueman nwg-look archlinux-xdg-menu hyprpicker nm-connection-editor hyprshot network-manager-applet breeze mako
+	sudo pacman -Syu --needed zsh git base-devel curl fastfetch starship hyprland waybar brightnessctl pavucontrol nwg-displays kitty ttf-firacode-nerd dolphin xdg-desktop-portal-hyprland xdg-desktop-portal playerctl wofi hyprpaper blueman nwg-look archlinux-xdg-menu hyprpicker nm-connection-editor hyprshot network-manager-applet breeze mako cliphist
 
 fi
 
